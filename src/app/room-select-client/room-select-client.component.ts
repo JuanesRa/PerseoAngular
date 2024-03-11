f7da79edf8fae7669b8d4d91e078c387b78a306e
jhonatanGe146 <jhonatangelvis@gmail.com>
2024-03-11 11:17:15 -0500

    Se valida habitaciones disponibles

--- src/app/room-select-client/room-select-client.component.ts
+++ src/app/room-select-client/room-select-client.component.ts
@@ -11,6 +11,7 @@ export class RoomSelectClientComponent implements OnInit {
   rooms: any[] = [];
   Typerooms: any[] = [];
   estados: any[] = [];
+  noRoomsAvailable: boolean = false; // Variable para controlar si no hay habitaciones disponibles
 
   // Variable para almacenar el ID del estado "Disponible"
   estadoDisponibleId: number = 0;
@@ -29,29 +30,30 @@ export class RoomSelectClientComponent implements OnInit {
 
         // Filtrar las habitaciones por el estado "Disponible"
         this.rooms = data.filter((item: any) => item.ESTADO_HABITACION_IDESTADOHABITACION == this.estadoDisponibleId);
-        console.log(this.rooms);
+        
+        if (this.rooms.length === 0) {
+          this.noRoomsAvailable = true; // Establecer la bandera en true si no hay habitaciones disponibles
+        } else {
+          // Obtener el tipo de habitación para cada habitación
+          this.roomService.getTypeRoom().subscribe((typeData) => {
+            this.Typerooms = typeData;
 
-        // Obtener el tipo de habitación para cada habitación
-        this.roomService.getTypeRoom().subscribe((typeData) => {
-          this.Typerooms = typeData;
-          console.log(this.Typerooms);
-        });
-
-        // Obtener foto de habitación para cada habitación
-        this.rooms.forEach((room)=> {
-          this.roomService.getTypeRoomById(room.TIPO_HABITACION_IDTIPOHABITACION).subscribe((statusData)=>{
-            room.foto = statusData.FOTO; // Asignar la foto al objeto de la habitación
-            room.tipo = statusData.TIPO_HABITACION
-            room.precio = statusData.PRECIOXNOCHE
-            room.descripcion = statusData.DESCRIPCION
-            room.cap_adultos = statusData.CANTIDAD_ADULTOS
-            room.cap_ninos = statusData.CANTIDAD_NINOS
+            // Obtener foto de habitación para cada habitación
+            this.rooms.forEach((room)=> {
+              this.roomService.getTypeRoomById(room.TIPO_HABITACION_IDTIPOHABITACION).subscribe((statusData)=>{
+                room.foto = statusData.FOTO; // Asignar la foto al objeto de la habitación
+                room.tipo = statusData.TIPO_HABITACION;
+                room.precio = statusData.PRECIOXNOCHE;
+                room.descripcion = statusData.DESCRIPCION;
+                room.cap_adultos = statusData.CANTIDAD_ADULTOS;
+                room.cap_ninos = statusData.CANTIDAD_NINOS;
+              });
+            });
           });
-        });
+        }
       });
     });
   }
-
 
   // Método para obtener el ID del estado "Disponible"
   getEstadoDisponibleId(): number {
@@ -62,5 +64,4 @@ export class RoomSelectClientComponent implements OnInit {
   redireccionarReservar(room: any): void {
     this.router.navigate(['/insertar-reserva', room.NROHABITACION], { state: { habitacion: room } });
   }
-
 }
