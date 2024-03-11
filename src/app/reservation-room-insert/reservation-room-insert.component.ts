741bcdd866b6b052af0d37b6936f3293efff1e68
JhonatanGel146 <115653316+jhonatanGe146@users.noreply.github.com>
2024-03-11 23:26:08 +0000

    Se realizo cambio de estado de habitacion automatico

--- src/app/reservation-room-insert/reservation-room-insert.component.ts
+++ src/app/reservation-room-insert/reservation-room-insert.component.ts
@@ -11,36 +11,91 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class ReservationRoomInsertComponent {
   formulario: FormGroup;
+  formularioActualizar: FormGroup;
   reservationId!: number;
   habitaciones: any[] = [];
+  estados: any[] = [];
+  // Variable para almacenar el ID del estado "Disponible"
+  estadoDisponibleId: number = 0;
+  // Variable para almacenar el ID del estado ID del estado "Ocupado"
+  estadoOcupadoId: number = 0;
 
-  constructor(private ReservationService: ReservationService,private RoomService:RoomService, private route: ActivatedRoute, private router: Router, public fb: FormBuilder) {
+  constructor(
+    private ReservationService: ReservationService,
+    private RoomService: RoomService,
+    private route: ActivatedRoute,
+    private router: Router,
+    public fb: FormBuilder
+  ) {
+    // Inicialización de los formularios
     this.formulario = this.fb.group({
-      HABITACION_NROHABITACION  : [null, Validators.required],
-      RESERVA_IDRESERVA : [null, [Validators.required, Validators.maxLength(30)]],
+      HABITACION_NROHABITACION: [null, Validators.required],
+      RESERVA_IDRESERVA: [null, [Validators.required, Validators.maxLength(30)]],
     });
-   }
+
+    this.formularioActualizar = this.fb.group({
+      ESTADO_HABITACION_IDESTADOHABITACION: [null, Validators.required],
+    });
+  }
 
   ngOnInit(): void {
-    // Obtener el ID del servicio de los parámetros de la ruta
-    this.reservationId =+this.route.snapshot.params['id'];
+    // Obtener el ID de la reserva desde los parámetros de la ruta
+    this.reservationId = +this.route.snapshot.params['id'];
 
     // Establecer el valor de RESERVA_IDRESERVA en el formulario
     this.formulario.patchValue({
       RESERVA_IDRESERVA: this.reservationId
     });
 
-    // Obtener los habitaciones
+    // Obtener las habitaciones y los estados de las habitaciones
     this.RoomService.getRooms().subscribe((data) => {
-      this.habitaciones = data;
-      console.log(this.habitaciones)
+      this.RoomService.getStatusRoom().subscribe((statusData) => {
+        // Asignar los estados obtenidos
+        this.estados = statusData;
+
+        // Obtener los IDs de los estados "Disponible" y "Ocupado"
+        this.estadoDisponibleId = this.getEstadoDisponibleId();
+        this.estadoOcupadoId = this.getEstadoOcupadoId();
+
+        // Filtrar las habitaciones disponibles
+        this.habitaciones = data.filter((item: any) => item.ESTADO_HABITACION_IDESTADOHABITACION == this.estadoDisponibleId);
+      });
     });
   }
 
+  // Método para crear una nueva habitación por reserva
   crearNuevoHabitacionXReserva(): void {
-    this.ReservationService.postReservationXRoom(this.formulario.value).subscribe((data) => {
-      this.router.navigate(['/lista-habitacion-reserva', this.reservationId]);
+    // Obtener la habitación seleccionada en el formulario
+    const habitacionSeleccionada = this.formulario.value.HABITACION_NROHABITACION;
+    const habitacion = this.habitaciones.find(hab => hab.NROHABITACION === habitacionSeleccionada);
+    if (habitacion) {
+      // Crear la reserva para la habitación seleccionada
+      this.ReservationService.postReservationXRoom(this.formulario.value).subscribe(() => {
+        // Actualizar el estado de la habitación a "Ocupado"
+        this.ActualizarEstadoHabitacion(habitacion);
+        this.router.navigate(['/lista-habitacion-reserva', this.reservationId]);
+      });
+    } else {
+      console.error('No se ha seleccionado ninguna habitación.');
+    }
+  }
+
+  // Método para actualizar el estado de la habitación a "Ocupado"
+  ActualizarEstadoHabitacion(habitacion: any): void {
+    const estadoId = this.estadoOcupadoId;
+    this.RoomService.patchRoom(habitacion.NROHABITACION, { ESTADO_HABITACION_IDESTADOHABITACION: estadoId }).subscribe(() => {
     });
   }
 
+  // Método para obtener el ID del estado "Disponible"
+  getEstadoDisponibleId(): number {
+    const estadoDisponible = this.estados.find((estado) => estado.TIPO_ESTADO == 'Disponible');
+    return estadoDisponible ? estadoDisponible.IDESTADOHABITACION : 0;
+  }
+
+  // Método para obtener el ID del estado "Ocupado"
+  getEstadoOcupadoId(): number {
+    const estadoOcupado = this.estados.find((estado) => estado.TIPO_ESTADO == 'Ocupado');
+    return estadoOcupado ? estadoOcupado.IDESTADOHABITACION : 0;
+  }
 }
--- src/app/reservation-room-select/reservation-room-select.component.ts
+++ src/app/reservation-room-select/reservation-room-select.component.ts
@@ -2,42 +2,60 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ReservationService } from '../services/reservation.service';
 import { RoomService } from '../services/room.service';
-
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reservation-room-select',
   templateUrl: './reservation-room-select.component.html',
   styleUrls: ['./reservation-room-select.component.css']
 })
-export class ReservationRoomSelectComponent {
+export class ReservationRoomSelectComponent implements OnInit {
   roomxreservation: any[] = [];
   reservationId!: number;
+  estados: any[] = [];
+  estadoDisponibleId: number = 0;
+  estadoOcupadoId: number = 0;
+  nroHab: number = 0;
+  formularioActualizar: FormGroup;
 
-  constructor(private ReservationService: ReservationService, private route: ActivatedRoute, private router: Router, private RoomService:RoomService) { }
+  constructor(
+    private ReservationService: ReservationService,
+    private router: Router,
+    private route: ActivatedRoute,
+    public fb: FormBuilder,
+    private RoomService: RoomService
+  ) {
+    this.formularioActualizar = this.fb.group({
+      ESTADO_HABITACION_IDESTADOHABITACION: [null, Validators.required],
+    });
+  }
 
   ngOnInit(): void {
-    // Obtener el ID de la Reserva
+    this.RoomService.getStatusRoom().subscribe((statusData) => {
+      // Asignar los estados obtenidos
+      this.estados = statusData
+
+      // Obtener los IDs de los estados "Disponible" y "Ocupado"
+      this.estadoDisponibleId = this.getEstadoDisponibleId();
+      this.estadoOcupadoId = this.getEstadoOcupadoId();
+    });
+
+
     this.reservationId = +this.route.snapshot.params['id'];
 
     this.ReservationService.getReservationXRoom().subscribe((data) => {
-      // Filtrar los registros según el valor de RESERVA_IDRESERVA
-      this.roomxreservation = data.filter((item: any) => item.RESERVA_IDRESERVA  == this.reservationId);
+      this.roomxreservation = data.filter((item: any) => item.RESERVA_IDRESERVA == this.reservationId);
 
-    this.roomxreservation.forEach((RoomXReservation) => {
-      // Traer habitaciones
-      this.RoomService.getRoomById(RoomXReservation.HABITACION_NROHABITACION).subscribe((statusData) => {
-        RoomXReservation.NroHabitacion = statusData.NROHABITACION;
-        RoomXReservation.IDTIPOHABITACION = statusData.TIPO_HABITACION_IDTIPOHABITACION ;
-        // Traer Tipo Habitación
-        this.RoomService.getTypeRoomById(RoomXReservation.IDTIPOHABITACION).subscribe((statusData) => {
-          RoomXReservation.tipo  = statusData.TIPO_HABITACION;
-          RoomXReservation.precio  = statusData.PRECIOXNOCHE;
-          console.log(this.roomxreservation)
-
+      this.roomxreservation.forEach((RoomXReservation) => {
+        this.RoomService.getRoomById(RoomXReservation.HABITACION_NROHABITACION).subscribe((statusData) => {
+          RoomXReservation.NroHabitacion = statusData.NROHABITACION;
+          RoomXReservation.IDTIPOHABITACION = statusData.TIPO_HABITACION_IDTIPOHABITACION;
+          this.RoomService.getTypeRoomById(RoomXReservation.IDTIPOHABITACION).subscribe((statusData) => {
+            RoomXReservation.tipo = statusData.TIPO_HABITACION;
+            RoomXReservation.precio = statusData.PRECIOXNOCHE;
+          });
         });
       });
-    });
     });
   }
 
@@ -47,9 +65,33 @@ export class ReservationRoomSelectComponent {
 
   eliminarHabitacionxReserva(guestxreserId: number): void {
     if (confirm('¿Está seguro de eliminar la Habitación?')) {
-      this.ReservationService.deleteReservationXRoom(guestxreserId).subscribe(() => {
-        window.location.reload();
+      this.ReservationService.getReservationXRoomById(guestxreserId).subscribe((data) => {
+        this.nroHab = data.HABITACION_NROHABITACION;
+        this.ActualizarEstadoHabitacion(this.nroHab);
+        // Luego de actualizar el estado de la habitación, eliminar la habitación de la reserva
+        this.ReservationService.deleteReservationXRoom(guestxreserId).subscribe(() => {
+          window.location.reload();
+        });
       });
     }
+  }
+
+  ActualizarEstadoHabitacion(nroHabitacion: number): void {
+    const estadoId = this.estadoDisponibleId;
+    this.RoomService.patchRoom(nroHabitacion, {ESTADO_HABITACION_IDESTADOHABITACION: estadoId }).subscribe(() => {
+      console.log('Estado de la habitación actualizado correctamente.');
+    }, error => {
+      console.error('Error al actualizar el estado de la habitación:', error);
+    });
+  }
+
+  getEstadoDisponibleId(): number {
+    const estadoDisponible = this.estados.find((estado) => estado.TIPO_ESTADO == 'Disponible');
+    return estadoDisponible ? estadoDisponible.IDESTADOHABITACION : 0;
+  }
+
+  getEstadoOcupadoId(): number {
+    const estadoOcupado = this.estados.find((estado) => estado.TIPO_ESTADO == 'Ocupado');
+    return estadoOcupado ? estadoOcupado.IDESTADOHABITACION : 0;
   }
 }
